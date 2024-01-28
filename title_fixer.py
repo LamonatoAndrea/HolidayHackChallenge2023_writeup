50cd758742ba74dbb7c0249dc4cc49e61d9418ea
TheDead <thedead@dellian>
2024-01-28 16:11:06 +0100

    Sun Jan 28 04:11:06 PM CET 2024

--- title_fixer.py
+++ title_fixer.py
@@ -1,8 +1,22 @@
 import os
 import re
+import urllib.parse
+import pprint
 
 directory = "."
 
+dirs = []
 for filename in (os.listdir(directory)):
-	if re.match(r'$\d+.*?', filename):
-		print (filename)
+	if re.match(r'^\d+.*?', filename):
+		dirs.append (filename)
+dirs.sort()
+
+dir_objs = []
+for d in dirs:
+	## [KringleCon Orientation](/01%20-%20KringleCon%20Orientation/README.md)
+	challenge_name = d[5:]
+	readme_url = "/{}/README.md".format(urllib.parse.quote(filename))
+	dir_obj = {'original': d, 'readme_url': readme_url, 'challenge_name': challenge_name}
+	dir_objs.append(dir_obj)
+
+	print ("## [{}]({})".format(challenge_name, readme_url))
