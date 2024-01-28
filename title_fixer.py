10001f1159479bde87b44a0245a0da96bfe57184
TheDead <thedead@dellian>
2024-01-28 16:17:37 +0100

    Sun Jan 28 04:17:37 PM CET 2024

--- title_fixer.py
+++ title_fixer.py
@@ -9,11 +9,12 @@ dirs = []
 for filename in (os.listdir(directory)):
 	if re.match(r'^\d+.*?', filename):
 		dirs.append (filename)
+dirs.append("/")
 dirs.sort()
+
 
 dir_objs = []
 for d in dirs:
-	## [KringleCon Orientation](/01%20-%20KringleCon%20Orientation/README.md)
 	challenge_name = d[5:]
 	readme_url = "/{}/README.md".format(urllib.parse.quote(d))
 	dir_obj = {'original': d, 'readme_url': readme_url, 'challenge_name': challenge_name}
